077e1971b3502cbf0d922fa400021e1898b18aca
HeckYea <david.yay06@hotmail.com>
2025-11-05 11:53:06 -0700

    in class examples, plus some python fun
    title

---
+++ In-Class Examples/Nov5.m
@@ -0,0 +1,65 @@
+% for k = 1:3
+%     5^k
+% end
+
+
+% for k = 1:5
+%     a(k) = k^2
+% end
+
+
+% for k = 1:5
+%     a = k^2
+% end
+
+
+% k = 1:5
+% a = k.^2
+
+
+% x = 1:0.01:100;
+% y = sin(x);
+% plot(x,y)
+
+
+% scores = [76, 45, 98, 97]
+% scoresMax = scores>90
+% count=sum(scoresMax)
+
+
+%%%%%% THIS ONE IS BETTER
+% scores = [76, 45, 98, 97];
+% 
+% count = 0;
+% for k = scores
+%     if k >= 90
+%         count = count+1;
+%     end
+% end
+% count
+
+
+
+% scores = [76, 45, 98, 97];
+% 
+% 
+% count = 0;
+% for k = scores
+%     if k >= 90
+%         count = count+1;
+%     end
+% end
+% disp(count)
+
+
+% scores = [76, 45, 98, 97];
+% scoreCheck = scores >= 90
+
+
+% scores = [76, 45, 98, 97];
+% length(find(scores>=90))
+
+
+
+
+
---
+++ In-Class Examples/Nov5.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+scores = np.array([88, 92, 97, 75])
+count = np.sum(scores >= 90)
+print(count)
